a774244a2bbd35f7faa8fd49395104bed06cd493
Linus <linuskmr.dev@gmail.com>
2022-09-20 11:27:16 +0200

    6.2 Simplifying the Ray-Sphere Intersection Code

--- src/sphere.rs
+++ src/sphere.rs
@@ -9,10 +9,10 @@ pub struct Sphere {
 impl Sphere {
 	pub fn hits(&self, ray: &Ray) -> Option<f64> {
 		let oc = ray.origin - self.center;
-		let a = ray.direction.dot(ray.direction);
-		let b = 2.0 * oc.dot(ray.direction);
-		let c = oc.dot(oc) - self.radius * self.radius;
-		let discriminant = b * b - 4.0 * a * c;
+		let a = ray.direction.squared_length();
+		let half_b = oc.dot(ray.direction);
+		let c = oc.squared_length() - self.radius * self.radius;
+		let discriminant = half_b * half_b - a * c;
 
 		if discriminant < 0.0 {
 			// If discriminant is negative, there are no real roots, so return false as ray misses sphere
@@ -21,8 +21,8 @@ impl Sphere {
 
 		// If discriminant is zero, there is one real root, so return true as ray hits sphere
 		// If discriminant is positive, there are two real roots, so return true as ray hits sphere
-		let t1 = (-b - discriminant.sqrt()) / (2.0 * a); // Way used by tutorial
-		let t2 = (-b + discriminant.sqrt()) / (2.0 * a);
+		let t1 = (-half_b - discriminant.sqrt()) / a; // Way used by tutorial
+		let t2 = (-half_b + discriminant.sqrt()) / a;
 		assert!(t1 < t2, "t1 (tutorial) should be less than t2");
 		Some(t1)
 	}
