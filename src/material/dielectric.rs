6dfb3535286b9e6974fe6b8422fca0c905f4e9e7
Linus <linuskmr.dev@gmail.com>
2022-09-22 10:24:47 +0200

    10.3 Total Internal Reflection

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -15,10 +15,19 @@ impl Material for Dielectric {
 			IntersectionSide::Outside => 1.0 / self.index_of_refraction,
 		};
 		let unit_direction = ray.direction.unit_vector();
-		let refracted = unit_direction.refract(hit.normal, refraction_ratio);
+		let cos_theta = f64::min((-unit_direction).dot(hit.normal), 1.0);
+		let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
+
+		let cannot_refract = refraction_ratio * sin_theta > 1.0;
+		let direction = if cannot_refract {
+			unit_direction.reflect(hit.normal)
+		} else {
+			unit_direction.refract(hit.normal, refraction_ratio)
+		};
+
 		let scattered = Ray {
 			origin: hit.point,
-			direction: refracted,
+			direction,
 		};
 		Some((scattered, attenuation_color))
 	}
